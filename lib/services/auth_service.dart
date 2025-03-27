dacf3fd32ccf63a5ac958fc07c051ed7c197a64d
Jordy <jordy.munoz11@gmail.com>
2025-03-27 11:15:16 -0500

    configuración adicional

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -5,7 +5,7 @@ class AuthService {
   final String baseUrl = 'https://tu-api.com';
 
   Future<Map<String, dynamic>> login(String email, String password) async {
-    final response = await http.post(
+    final response = await http.post( 
       Uri.parse('$baseUrl/login'),
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode({'email': email, 'password': password}),
