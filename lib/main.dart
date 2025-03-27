cd50253252af9f4cf82eca85d9996c5757ccd92c
Jordy <jordy.munoz11@gmail.com>
2025-03-27 14:12:25 -0500

    implementacion firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,70 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tienda_online/firebase_options.dart';
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+    
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
-  final prefs = await SharedPreferences.getInstance();
-  final isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+  // final prefs = await SharedPreferences.getInstance();
+  // final isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
 
-  runApp(MyApp(isLoggedIn: isLoggedIn));
+  // runApp(MyApp(isLoggedIn: isLoggedIn));
+  runApp(const MyApp(isLoggedIn: false,));
 }
 
 class MyApp extends StatelessWidget {
-  final bool isLoggedIn;
+  const MyApp({super.key, required bool isLoggedIn});
+  
+  // final bool isLoggedIn;
 
-  const MyApp({Key? key, required this.isLoggedIn}) : super(key: key);
+  // const MyApp({Key? key, required this.isLoggedIn}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+      final providers = [EmailAuthProvider()];
+
+    void onSignedIn() {
+      Navigator.pushReplacementNamed(context, '/profile');
+    }
+
     return MaterialApp(
-      title: 'Tienda de Motocicletas',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: isLoggedIn ? HomeScreen() : LoginScreen(),
+      initialRoute: FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/profile',
+      routes: {
+        '/sign-in': (context) {
+          return SignInScreen(
+            providers: providers,
+            actions: [
+              AuthStateChangeAction<UserCreated>((context, state) {
+                Navigator.pushReplacementNamed(context, '/profile');
+                
+              }),
+              AuthStateChangeAction<SignedIn>((context, state) {
+                Navigator.pushReplacementNamed(context, '/profile');
+                
+              }),
+            ],
+          );
+        },
+        '/profile': (context) {
+          return ProfileScreen(
+            providers: providers,
+            actions: [
+              SignedOutAction((context) {
+                Navigator.pushReplacementNamed(context, '/sign-in');
+              }),
+            ],
+          );
+        },
+      },
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tienda_online/main.dart';
 
 void main() {
+ 
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp(isLoggedIn: false));
